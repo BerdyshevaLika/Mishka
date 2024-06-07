d3f86711428efedea117f5ac523c50a1f876dd60
BerdyshevaLika <112309617+BerdyshevaLika@users.noreply.github.com>
2024-06-07 21:00:16 +0700

    Add files via upload

---
+++ js/burger-menu.js
@@ -0,0 +1,1 @@
+const mainHeaderToggle=document.querySelector(".main-header__toggle"),mainHeader=document.querySelector(".main-header");mainHeader.classList.remove("no-js"),mainHeaderToggle.addEventListener("click",(()=>mainHeader.classList.toggle("is-open")));
